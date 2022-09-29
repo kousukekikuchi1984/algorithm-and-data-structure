f2ffe2bc7938f5e2e9adfd29ed5196e50c3e6817
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2022-09-29 23:22:37 +0900

    added number 753

--- src/chapter4/recursive.rs
+++ src/chapter4/recursive.rs
@@ -28,10 +28,32 @@ pub fn memorized_tribonacci(n: u32) -> u32 {
     return val as u32;
 }
 
+pub fn number_753(k: u32) -> u32 {
+    let mut count: u32 = 0;
+    fn _number_753(k: u32, prev_num: u32, count: &mut u32) {
+        let prev_str = prev_num.to_string();
+        println!("{}", prev_str);
+        if prev_num > k {
+            return; // stop iteration
+        }
+        if prev_str.contains("3") && prev_str.contains("5") && prev_str.contains("7") {
+            *count += 1;
+        }
+        _number_753(k, prev_num * 10 + 3, count);
+        _number_753(k, prev_num * 10 + 5, count);
+        _number_753(k, prev_num * 10 + 7, count);
+    }
+
+    _number_753(k, 0, &mut count);
+    return count as u32;
+}
+
 #[cfg(test)]
 mod tests {
 
-    use super::tribonacci;
+    use crate::chapter4::recursive::memorized_tribonacci;
+
+    use super::{number_753, tribonacci};
 
     #[test]
     fn test_tribonacci() {
@@ -42,8 +64,14 @@ mod tests {
 
     #[test]
     fn test_memorized_tribonacci() {
-        assert_eq!(tribonacci(4), 2);
-        assert_eq!(tribonacci(9), 44);
-        assert_eq!(tribonacci(19), 19513);
+        assert_eq!(memorized_tribonacci(4), 2);
+        assert_eq!(memorized_tribonacci(9), 44);
+        assert_eq!(memorized_tribonacci(19), 19513);
+    }
+
+    #[test]
+    fn test_number_753() {
+        assert_eq!(number_753(575), 4);
+        assert_eq!(number_753(3600), 13);
     }
 }
