1bff7fea173f3785c2b0349deb254cd3620be920
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2022-10-04 07:19:44 +0900

    subset sum

--- src/chapter5/dynamic_programming.rs
+++ src/chapter5/dynamic_programming.rs
@@ -22,9 +22,34 @@ fn summer_holiday_happiness(a: Vec<i32>, b: Vec<i32>, c: Vec<i32>) -> i32 {
     return *dp[a.len() - 1].iter().max().unwrap();
 }
 
+fn subset_sum(nums: Vec<i32>, w: i32) -> bool {
+    let mut dp = vec![vec![0; w as usize + 1]; nums.len()];
+
+    for n in 0..nums.len() {
+        for val in 0..w as usize {
+            if n == 0 {
+                if val == 0 {
+                    dp[n][val] = 1;
+                } else if val == nums[n] as usize {
+                    dp[n][val] = 1;
+                }
+            } else {
+                if dp[n - 1][val] == 1 {
+                    dp[n][val] = 1;
+                    let selected = dp[n - 1][val] + nums[n];
+                    if selected <= w {
+                        dp[n][selected as usize] = 1;
+                    }
+                }
+            }
+        }
+    }
+    dp[nums.len() - 1][w as usize] == 1
+}
+
 #[cfg(test)]
 mod tests {
-    use super::summer_holiday_happiness;
+    use super::{subset_sum, summer_holiday_happiness};
 
     #[test]
     fn test_summer_holiday_happiness() {
@@ -32,5 +57,11 @@ mod tests {
         let swimming = vec![2, 3, 1];
         let homework = vec![1, 1, 1];
         assert_eq!(summer_holiday_happiness(swimming, bugs, homework), 7);
+    }
+
+    #[test]
+    fn test_subset_sum() {
+        assert_eq!(subset_sum(vec![1, 3, 8], 9), true);
+        assert_eq!(subset_sum(vec![1, 3, 7], 9), false);
     }
 }
