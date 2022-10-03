0d4740b137c2b6c0aa72188b135b4c86197d2324
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2022-10-03 23:35:27 +0900

    fixed the code

--- src/chapter5/dynamic_programming.rs
+++ src/chapter5/dynamic_programming.rs
@@ -1,31 +1,25 @@
 fn summer_holiday_happiness(a: Vec<i32>, b: Vec<i32>, c: Vec<i32>) -> i32 {
-    fn relaxzation(a: &mut i32, b: i32) {
-        if *a < b {
-            *a = b;
-        }
-    }
     // a -> happiness of swimming
     // b -> happiness of bug catching
     // c -> happiness of doing homework
     // n -> n days
-    let mut dp = vec![std::i32::MIN; a.len()];
-    let first_values = vec![a[0], b[0], c[0]];
-    let first_happiness = first_values.iter().max().unwrap();
-    dp[0] = *first_happiness;
-
-    for n in 1..a.len() {
-        let hp = vec![a[n], b[n], c[n]];
-        let today_happiness = hp.iter().max().unwrap();
-        let total_happiness: i32;
-        if n == 1 {
-            total_happiness = dp[0] + today_happiness;
-        } else {
-            total_happiness = dp[n - 1] + today_happiness;
+    // same category cannot be used consequcutive days
+    let values = vec![&a, &b, &c];
+    let mut dp = vec![vec![std::i32::MIN; 3]; a.len()];
+    for i in 0..a.len() {
+        for j in 0..3 {
+            if i == 0 {
+                dp[i][j] = values[j][i];
+            } else {
+                // remove same index;
+                let mut val = values[j].to_vec();
+                val.remove(i);
+                let happiness = val.iter().max().unwrap();
+                dp[i][j] = dp[i - 1][j] + happiness;
+            }
         }
-        relaxzation(&mut dp[n], total_happiness);
     }
-
-    return dp[a.len() - 1];
+    return *dp[a.len() - 1].iter().max().unwrap();
 }
 
 #[cfg(test)]
@@ -37,6 +31,6 @@ mod tests {
         let bugs = vec![1, 2, 3];
         let swimming = vec![2, 3, 1];
         let homework = vec![1, 1, 1];
-        assert_eq!(summer_holiday_happiness(swimming, bugs, homework), 8);
+        assert_eq!(summer_holiday_happiness(swimming, bugs, homework), 7);
     }
 }
