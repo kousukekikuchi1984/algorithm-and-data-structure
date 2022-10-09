8a97b62524263b6e9db0e3526cbcb7c381ee611e
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2022-10-09 21:26:50 +0900

    added sum of three intergers

--- src/exercises/chapter6.rs
+++ src/exercises/chapter6.rs
@@ -1,4 +1,5 @@
 use std::cmp::Ordering;
+use superslice::Ext;
 
 pub fn nth_rank(nums: Vec<u32>) -> Vec<usize> {
     fn _binary_search(nums: Vec<u32>, target_num: u32, left: isize, right: isize) -> Option<usize> {
@@ -13,6 +14,7 @@ pub fn nth_rank(nums: Vec<u32>) -> Vec<usize> {
             Ordering::Greater => _binary_search(nums, target_num, mid + 1, right),
         }
     }
+
     let mut copied = nums.to_vec();
     let mut results = vec![0; nums.len()];
     copied.sort();
@@ -23,12 +25,34 @@ pub fn nth_rank(nums: Vec<u32>) -> Vec<usize> {
     return results;
 }
 
+pub fn sum_of_three_sequences(mut a: Vec<u32>, mut b: Vec<u32>, mut c: Vec<u32>) -> u32 {
+    a.sort();
+    b.sort();
+    c.sort();
+
+    b.iter()
+        .map(|b| a.lower_bound(&b) * (a.len() - c.upper_bound(&b)))
+        .sum::<usize>() as u32
+}
+
 #[cfg(test)]
 mod tests {
-    use super::nth_rank;
+    use super::{nth_rank, sum_of_three_sequences};
 
     #[test]
     fn test_nth_rank() {
         assert_eq!(nth_rank(vec![12, 43, 7, 15, 9]), vec![2, 4, 0, 3, 1]);
+    }
+
+    #[test]
+    fn test_sum_of_three_sequenes() {
+        assert_eq!(
+            sum_of_three_sequences(vec![1, 5], vec![2, 4], vec![3, 6]),
+            3
+        );
+        assert_eq!(
+            sum_of_three_sequences(vec![1, 1, 1], vec![2, 2, 2], vec![3, 3, 3]),
+            27
+        );
     }
 }
