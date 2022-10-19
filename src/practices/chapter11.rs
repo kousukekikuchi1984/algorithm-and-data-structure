786503414058dc3a71a4aca941479a61305cb5a1
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2022-10-20 07:36:01 +0900

    UnionFind should be implemented by myself to calculate size

--- src/practices/chapter11.rs
+++ src/practices/chapter11.rs
@@ -29,6 +29,23 @@ pub fn find_bridges(edge: Vec<Edge>, nodes: usize) -> u32 {
     result
 }
 
+pub fn decayed_bridges(edges: Vec<Edge>, nodes: usize) -> Vec<usize> {
+    let mut result = vec![];
+    let mut ans = nodes * (nodes - 1) / 2;
+    let mut ufind: UnionFind<u32> = UnionFind::new(nodes);
+    for m in (0..nodes).rev() {
+        result[m] = ans;
+        if ufind.find(edges[m].from) != ufind.find(edges[m].to) {
+            // The UnionFild has no get_size functionality.
+            // Also, private rank variable which is equivalent to size
+            // is not acccessed.
+            // let size_from = ufind.rank[ufind.find(edges[m].from)];
+        }
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod test {
     use super::{find_bridges, Edge};
@@ -44,4 +61,8 @@ mod test {
         ];
         assert_eq!(find_bridges(bridges, 6), 5);
     }
+
+    #[test]
+    #[ignore]
+    fn test_decayed_bridges() {}
 }
