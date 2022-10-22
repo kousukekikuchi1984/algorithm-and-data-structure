fddb89101ffa4fe72e91b89f2adc8c9457140c87
Kousuke Kikuchi <kousukekikuchi1984@gmail.com>
2022-10-23 07:50:18 +0900

    wip merge sort

--- src/exercises/chapter12.rs
+++ src/exercises/chapter12.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 pub fn insert_sort(mut v: Vec<u32>) -> Vec<u32> {
     println!("{:?}", v);
     for i in 1..v.len() {
@@ -10,13 +12,52 @@ pub fn insert_sort(mut v: Vec<u32>) -> Vec<u32> {
     v
 }
 
+fn merge_sort<T: Ord + Copy + Debug>(data: &mut [T]) {
+    if data.len() <= 1 {
+        return;
+    }
+
+    let mut right = data.len();
+    let middle = match right % 2 {
+        0 => right / 2,
+        1 => (right - 1) / 2,
+        _ => panic!(),
+    };
+    let mut left = 0;
+
+    merge_sort(&mut data[left..middle]);
+    merge_sort(&mut data[middle..right]);
+
+    let mut v = data.to_vec();
+    v[middle..right].reverse();
+
+    println!("{:?}", v);
+
+    for i in 0..right - 1 {
+        if v[left] > v[right] {
+            data[i] = v[right];
+            right -= 1;
+        } else {
+            data[i] = v[left];
+            left += 1;
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
 
-    use super::insert_sort;
+    use super::{insert_sort, merge_sort};
 
     #[test]
     fn test_insert_sort() {
         assert_eq!(insert_sort(vec![1, 5, 2, 4, 3]), vec![1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn test_merge_sort() {
+        let mut v = vec![1, 5, 2, 4, 3];
+        merge_sort(&mut v);
+        assert_eq!(&mut v, &mut vec![1, 2, 3, 4, 5]);
     }
 }
